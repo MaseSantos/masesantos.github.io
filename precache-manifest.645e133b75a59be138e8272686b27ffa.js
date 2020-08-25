8ede2b86b6a2962bfc9da9b0a858e500bcceb964
Mase Santos <hola@masesantos.work>
2020-08-24 20:32:54 -0500

    Updates

---
+++ precache-manifest.645e133b75a59be138e8272686b27ffa.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad33d82f5a4bd0d64ec1820b32e60b27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70a44e5c738e5a7f638f",
+    "url": "/static/css/main.de01be96.chunk.css"
+  },
+  {
+    "revision": "f763d89ad569ceccd933",
+    "url": "/static/js/2.f3c73fb5.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.f3c73fb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70a44e5c738e5a7f638f",
+    "url": "/static/js/main.b4908310.chunk.js"
+  },
+  {
+    "revision": "4cb321c27cb17a58292e",
+    "url": "/static/js/runtime-main.46c63bc0.js"
+  },
+  {
+    "revision": "93e39b24699c99f2329729fbee37988a",
+    "url": "/static/media/social-icons-01.93e39b24.png"
+  },
+  {
+    "revision": "767a7d77eb4c67b30b484d05a0217c60",
+    "url": "/static/media/social-icons-03.767a7d77.png"
+  },
+  {
+    "revision": "37a7dd9c5f4b8d16b9b393a0457768dc",
+    "url": "/static/media/social-icons-05.37a7dd9c.png"
+  }
+]);
